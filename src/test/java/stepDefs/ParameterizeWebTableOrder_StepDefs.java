ba72f2fbdcd5667c4bf24090b4879d603bf7aed9
begenchbegenjov <bgnch23@gmail.com>
2022-04-01 01:09:23 -0500

    organize folders2

--- src/test/java/stepDefs/ParameterizeWebTableOrder_StepDefs.java
+++ src/test/java/stepDefs/ParameterizeWebTableOrder_StepDefs.java
@@ -1,84 +1,84 @@
-package stepDefs;
-
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.junit.Assert;
-import org.openqa.selenium.WebElement;
-import pages.OrderPage;
-import pages.ViewAllOrdersPage;
-
-public class ParameterizeWebTableOrder_StepDefs {
-
-    OrderPage orderPage = new OrderPage();
-    ViewAllOrdersPage viewAllOrdersPage = new ViewAllOrdersPage();
-    String expectedCostumerName;
-
-    @When("user selects product type {int}")
-    public void userSelectsProductType(Integer int1) {
-        orderPage.getProductAsSelect().selectByIndex(int1);
-
-    }
-
-    @When("user enters quantity {int}")
-    public void userEntersQuantity(Integer int1) {
-        orderPage.quantityInput.sendKeys(int1.toString());
-    }
-
-    @When("user enters customer name {string}")
-    public void userEntersCustomerName(String string) {
-        expectedCostumerName = string;
-        orderPage.nameInput.sendKeys(string);
-
-    }
-
-    @When("user enters street {string}")
-    public void userEntersStreet(String string) {
-        orderPage.streetInput.sendKeys(string);
-
-    }
-
-    @When("user enters city {string}")
-    public void userEntersCity(String string) {
-
-        orderPage.cityInput.sendKeys(string);
-    }
-
-    @When("user enters state {string}")
-    public void userEntersState(String string) {
-
-        orderPage.stateInput.sendKeys(string);
-
-    }
-
-    @When("user enter zip {int}")
-    public void userEnterZip(Integer int1) {
-        orderPage.zipInput.sendKeys(int1.toString());
-    }
-
-    @When("user selects card type {string}")
-    public void userSelectsCardType(String string) {
-
-        orderPage.getCardTypeByValue(orderPage.cardInputs, string).click();
-    }
-
-    @When("user enters card number {string}")
-    public void userEntersCardNumber(String str) {
-        orderPage.cardNoInput.sendKeys(str);
-    }
-
-    @When("user enters card expiry date {string}")
-    public void userEntersCardExpiryDate(String string) {
-        orderPage.cardExpInput.sendKeys(string);
-    }
-
-    @Then("user should see customer name in the table on *view all orders* page")
-    public void userShouldSeeCustomerNameInTheTableOnViewAllOrdersPage() {
-
-        String actualCostumerName = viewAllOrdersPage.getLastOrderName();
-        Assert.assertEquals(expectedCostumerName, actualCostumerName);
-
-
-    }
-
-}
+//package stepDefs;
+//
+//import io.cucumber.java.en.And;
+//import io.cucumber.java.en.Then;
+//import io.cucumber.java.en.When;
+//import org.junit.Assert;
+//import org.openqa.selenium.WebElement;
+//import pages.OrderPage;
+//import pages.ViewAllOrdersPage;
+//
+//public class ParameterizeWebTableOrder_StepDefs {
+//
+//    OrderPage orderPage = new OrderPage();
+//    ViewAllOrdersPage viewAllOrdersPage = new ViewAllOrdersPage();
+//    String expectedCostumerName;
+//
+//    @When("user selects product type {int}")
+//    public void userSelectsProductType(Integer int1) {
+//        orderPage.getProductAsSelect().selectByIndex(int1);
+//
+//    }
+//
+//    @When("user enters quantity {int}")
+//    public void userEntersQuantity(Integer int1) {
+//        orderPage.quantityInput.sendKeys(int1.toString());
+//    }
+//
+//    @When("user enters customer name {string}")
+//    public void userEntersCustomerName(String string) {
+//        expectedCostumerName = string;
+//        orderPage.nameInput.sendKeys(string);
+//
+//    }
+//
+//    @When("user enters street {string}")
+//    public void userEntersStreet(String string) {
+//        orderPage.streetInput.sendKeys(string);
+//
+//    }
+//
+//    @When("user enters city {string}")
+//    public void userEntersCity(String string) {
+//
+//        orderPage.cityInput.sendKeys(string);
+//    }
+//
+//    @When("user enters state {string}")
+//    public void userEntersState(String string) {
+//
+//        orderPage.stateInput.sendKeys(string);
+//
+//    }
+//
+//    @When("user enter zip {int}")
+//    public void userEnterZip(Integer int1) {
+//        orderPage.zipInput.sendKeys(int1.toString());
+//    }
+//
+//    @When("user selects card type {string}")
+//    public void userSelectsCardType(String string) {
+//
+//        orderPage.getCardTypeByValue(orderPage.cardInputs, string).click();
+//    }
+//
+//    @When("user enters card number {string}")
+//    public void userEntersCardNumber(String str) {
+//        orderPage.cardNoInput.sendKeys(str);
+//    }
+//
+//    @When("user enters card expiry date {string}")
+//    public void userEntersCardExpiryDate(String string) {
+//        orderPage.cardExpInput.sendKeys(string);
+//    }
+//
+//    @Then("user should see customer name in the table on *view all orders* page")
+//    public void userShouldSeeCustomerNameInTheTableOnViewAllOrdersPage() {
+//
+//        String actualCostumerName = viewAllOrdersPage.getLastOrderName();
+//        Assert.assertEquals(expectedCostumerName, actualCostumerName);
+//
+//
+//    }
+//
+//}
--- src/test/resources/features/parameterizeWebTableOrder.feature
+++ src/test/resources/features/parameterizeWebTableOrder.feature
@@ -1,34 +1,34 @@
-@wip2
-Feature: Web table user order feature multiple parameters
-
-  TC:02
-
-  Background: user logged in and user in order page
-    Given user in order page
-
-  Scenario Outline: user should be able to place order and order seen in List of All Orders Table
-
-    When user selects product type <productType>
-    And user enters quantity <quantity>
-    And user enters customer name "<customerName>"
-    And user enters street "<streetName>"
-    And user enters city "<cityName>"
-    And user enters state "<stateName>"
-    And user enter zip <zipCode>
-    And user selects card type "<cardType>"
-    And user enters card number "<cardNumber>"
-    And user enters card expiry date "<expiry/date>"
-    And user clicks process order button
-    Then user should see customer name in the table on *view all orders* page
-
-    Examples:
-      | productType | quantity | customerName   | streetName      | cityName       | stateName | zipCode | cardType         | cardNumber   | expiry/date |
-      | 1           | 1        | Mila Kunis     | Hamburg st      | Belgrade       | Sting     | 96012   | Visa             | 123456789012 | 12/27       |
-      | 2           | 2        | Jannet Jackson | Asik koy kovesi | London         | Adaban    | 90873   | MasterCard       | 123478901256 | 06/29       |
-      | 0           | 3        | Apple Cucumber | Lincoln street  | Paris          | Bakers    | 67543   | American Express | 890112345672 | 02/28       |
-      | 1           | 4        | Sergio Ramos   | Martin Luther   | Constantinople | Jonson    | 99764   | MasterCard       | 123457890162 | 11/29       |
-      | 2           | 5        | Adalat Sadakat | Michael Jason   | Tashkent       | Hunt      | 34684   | Visa             | 901234567812 | 10/23       |
-      | 1           | 6        | Selmia Kamila  | Lebron Kamin    | Kiev           | Ming      | 98763   | MasterCard       | 156789012234 | 09/25       |
-      | 0           | 7        | Jaren Gurdova  | Saddam Ali      | Ashgabat       | Maliska   | 99712   | American Express | 789012345612 | 02/25       |
-
-
+#@wip2
+#Feature: Web table user order feature multiple parameters
+#
+#  TC:02
+#
+#  Background: user logged in and user in order page
+#    Given user in order page
+#
+#  Scenario Outline: user should be able to place order and order seen in List of All Orders Table
+#
+#    When user selects product type <productType>
+#    And user enters quantity <quantity>
+#    And user enters customer name "<customerName>"
+#    And user enters street "<streetName>"
+#    And user enters city "<cityName>"
+#    And user enters state "<stateName>"
+#    And user enter zip <zipCode>
+#    And user selects card type "<cardType>"
+#    And user enters card number "<cardNumber>"
+#    And user enters card expiry date "<expiry/date>"
+#    And user clicks process order button
+#    Then user should see customer name in the table on *view all orders* page
+#
+#    Examples:
+#      | productType | quantity | customerName   | streetName      | cityName       | stateName | zipCode | cardType         | cardNumber   | expiry/date |
+#      | 1           | 1        | Mila Kunis     | Hamburg st      | Belgrade       | Sting     | 96012   | Visa             | 123456789012 | 12/27       |
+#      | 2           | 2        | Jannet Jackson | Asik koy kovesi | London         | Adaban    | 90873   | MasterCard       | 123478901256 | 06/29       |
+#      | 0           | 3        | Apple Cucumber | Lincoln street  | Paris          | Bakers    | 67543   | American Express | 890112345672 | 02/28       |
+#      | 1           | 4        | Sergio Ramos   | Martin Luther   | Constantinople | Jonson    | 99764   | MasterCard       | 123457890162 | 11/29       |
+#      | 2           | 5        | Adalat Sadakat | Michael Jason   | Tashkent       | Hunt      | 34684   | Visa             | 901234567812 | 10/23       |
+#      | 1           | 6        | Selmia Kamila  | Lebron Kamin    | Kiev           | Ming      | 98763   | MasterCard       | 156789012234 | 09/25       |
+#      | 0           | 7        | Jaren Gurdova  | Saddam Ali      | Ashgabat       | Maliska   | 99712   | American Express | 789012345612 | 02/25       |
+#
+#
